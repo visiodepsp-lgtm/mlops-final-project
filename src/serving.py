d23d2fbcf4f4cfb909f0b0e83f589f217be6c49e
visiodepsp-lgtm <visiodepsp@gmail.com>
2026-02-27 22:39:55 -0500

    Update serving.py for model loading and HTML response
    Refactor Flask app to use pickle for model loading and add HTML rendering for predictions.

--- src/serving.py
+++ src/serving.py
@@ -1,22 +1,41 @@
-from flask import Flask, request, jsonify
-import joblib
-import pandas as pd
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
 
-app = Flask(__name__)
+app = Flask("My Model API") #Initialize the flask App
+model = pickle.load(open('models/model.pkl', 'rb'))
 
-# Cargar modelo
-model = joblib.load("models/best_diabetes_model.pkl")
-
-@app.route("/")
+@app.route('/')
 def home():
-    return "Diabetes Prediction API is running!"
+    return render_template('index.html')
 
-@app.route("/predict", methods=["POST"])
+@app.route('/predict',methods=['POST'])
 def predict():
-    data = request.get_json()
-    input_df = pd.DataFrame([data])
-    prediction = model.predict(input_df)
-    return jsonify({"prediction": int(prediction[0])})
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+@app.route('/predict2',methods=['POST'])
+def predict2():
+    features_dict = request.get_json()
+    print(f"features dict: {features_dict}")
+    input_features = [ features_dict.get("experience_score"), features_dict.get("test_score"), features_dict.get("interview_score")]
+    #int_features = [int(x) for x in ]
+    final_features = [np.array(input_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+    return jsonify({"prediction": output})
+    #return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    #app.run(debug=True)
+    app.run()
