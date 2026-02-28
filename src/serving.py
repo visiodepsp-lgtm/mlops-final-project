3ec20570f7feae20012788bda14d88b763d52dea
visiodepsp-lgtm <visiodepsp@gmail.com>
2026-02-27 22:36:39 -0500

    Refactor serving.py to use joblib and pandas

--- src/serving.py
+++ src/serving.py
@@ -1,41 +1,22 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
 
-app = Flask("My Model API") #Initialize the flask App
-model = pickle.load(open('models/model.pkl', 'rb'))
+app = Flask(__name__)
 
-@app.route('/')
+# Cargar modelo
+model = joblib.load("models/best_diabetes_model.pkl")
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return "Diabetes Prediction API is running!"
 
-@app.route('/predict',methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
-@app.route('/predict2',methods=['POST'])
-def predict2():
-    features_dict = request.get_json()
-    print(f"features dict: {features_dict}")
-    input_features = [ features_dict.get("experience_score"), features_dict.get("test_score"), features_dict.get("interview_score")]
-    #int_features = [int(x) for x in ]
-    final_features = [np.array(input_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-    return jsonify({"prediction": output})
-    #return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
+    data = request.get_json()
+    input_df = pd.DataFrame([data])
+    prediction = model.predict(input_df)
+    return jsonify({"prediction": int(prediction[0])})
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
