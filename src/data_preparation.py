e89126f2c1531cec5f722d251c89e158ca04191d
visiodepsp-lgtm <visiodepsp@gmail.com>
2026-02-27 22:35:04 -0500

    Update data_preparation.py

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,13 +1,24 @@
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
-def data_preparation(raw_dataset_file: str, train_dataset_file: str):
-    #TODO
-    pass
-    #df.to_csv(  train_dataset_file)
+def load_and_prepare_data(path):
 
+    df = pd.read_csv(path)
 
+    X = df.drop("diabetes", axis=1)
+    y = df["diabetes"]
 
-if __name__ == "__main__":
-    data_preparation("data/raw/hiring.csv", "data/training/hiring_training.csv")
+    categorical_cols = ["gender", "smoking_history"]
+    numerical_cols = [col for col in X.columns if col not in categorical_cols]
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_cols),
+            ("num", "passthrough", numerical_cols)
+        ]
+    )
+
+    return X, y, preprocessor
 
 
